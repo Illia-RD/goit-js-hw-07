db5acfa802dc15253ba3b9b6a47e5f3a4acdb386
Illia <2201illia@gmail.com>
2022-08-21 02:47:38 +0300

    make changes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,33 +9,15 @@ gallery.insertAdjacentHTML(
   'afterbegin',
   galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+      return `
+    <a class = "gallery__item" href= "${original}">
+    <img class = "gallery__image" src = ${preview} alt = "${description}">
+    </a>
+    `;
     })
     .join('')
 );
-
-function openModalImg(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  //   const instance = basicLightbox.create(
-  //     `
-  //       <img src="${event.target.getAttribute('data-source')}">,
-  //   `,
-  //     {
-  //       onShow: () => window.addEventListener('keydown', onEsc),
-  //       onClose: () => window.removeEventListener('keydown', onEsc),
-  //     }
-  //   );
-  //   function onEsc(event) {
-  //     if (event.code === 'Escape') {
-  //       instance.close();
-  //     }
-  //   }
-  //   instance.show();
-}
-
-gallery.addEventListener('click', openModalImg);
+const lightbox = new SimpleLightbox('.gallery .gallery__item ', {
+  captionsData: 'alt',
+  captionDelay: 180,
+});
