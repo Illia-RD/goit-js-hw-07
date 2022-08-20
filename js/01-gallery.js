c6853e1b76bc6a2cf73fcc38d11d6dedd372cdd4
Illia <2201illia@gmail.com>
2022-08-21 01:42:07 +0300

    make changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+/*----------------------------------------------------------------------------------------------*/
 const gallery = document.querySelector('.gallery');
+
 gallery.insertAdjacentHTML(
   'afterbegin',
   galleryItems
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt="${description}"
+      alt="${description},"
     />
   </a>
 </div>`;
@@ -21,5 +22,24 @@ gallery.insertAdjacentHTML(
     .join('')
 );
 
-gallery.addEventListener('click');
-function openModalImg(event) {}
+function openModalImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+      <img src="${event.target.getAttribute('data-source')}">,
+  `,
+    {
+      onShow: () => window.addEventListener('keydown', onEsc),
+      onClose: () => window.removeEventListener('keydown', onEsc),
+    }
+  );
+  const onEsc = event => {
+    if (event.code === 'Escape') instance.close();
+  };
+  instance.show();
+}
+
+gallery.addEventListener('click', openModalImg);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+/*------------------------------------------------------------------------------------------------------------------------------*/
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('')
+);
+
+function openModalImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  //   const instance = basicLightbox.create(
+  //     `
+  //       <img src="${event.target.getAttribute('data-source')}">,
+  //   `,
+  //     {
+  //       onShow: () => window.addEventListener('keydown', onEsc),
+  //       onClose: () => window.removeEventListener('keydown', onEsc),
+  //     }
+  //   );
+  //   function onEsc(event) {
+  //     if (event.code === 'Escape') {
+  //       instance.close();
+  //     }
+  //   }
+  //   instance.show();
+}
+
+gallery.addEventListener('click', openModalImg);
