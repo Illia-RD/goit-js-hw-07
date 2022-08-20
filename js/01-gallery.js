88c5ed323f2a3c2cbe124bf656410b20655945ef
Illia <2201illia@gmail.com>
2022-08-20 23:14:59 +0300

    make changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('')
+);
+
+gallery.addEventListener('click');
+function openModalImg(event) {}
